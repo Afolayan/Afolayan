9c6ff93b3a1aa1c1cf95efc9e72798b35fb68d4b
Jephthah Afolayan <afolayanseyi@gmail.com>
2026-02-26 15:03:25 +0100

    Initialize project structure and configuration for resume/blog

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Jephthah M. | Digital Resume';
+export const SITE_DESCRIPTION = 'Passionate Full-Stack Developer & Technical Blogger building modern web experiences.';
